dda43ad80f7e403163671093b0b1f53b3675764c
nurmhm <nurmd7228@gmail.com>
2025-06-23 23:51:23 +0600

    hero add

--- components/Custom/HeroSection.tsx
+++ components/Custom/HeroSection.tsx
@@ -4,7 +4,7 @@ function HeroSection() {
   return (
     <section className='bg-gradient-to-r from-blue-600 to-blue-800 text-white py-16'>
       <div className='text-center mb-8'>
-            
+            this is hero section
       </div>
     </section>
   )
