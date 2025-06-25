7c1d141feca1f6e56ede83a77b064c05102df5df
mdruhulamin5347 <mdruhulamin534793@gmail.com>
2025-06-26 00:53:21 +0600

    commit

---
+++ app/HomePageClient.tsx
@@ -0,0 +1,14 @@
+import HeroSection from '@/components/Custom/HeroSection'
+import Navber from '@/components/Custom/Navber'
+import React from 'react' 
+
+function HomePageClient() {
+  return (
+    <div className='min-h-screen bg-gray-50'>
+      <Navber/>
+
+    </div>
+  )
+}
+
+export default HomePageClient
